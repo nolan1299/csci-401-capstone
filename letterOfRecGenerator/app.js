793b34ccfbb97e569bad487fd46dea1a52913224
Jerry Tejada <jmtejada@usc.edu>
2019-12-15 21:17:50 -0800

    Update app.js

--- letterOfRecGenerator/app.js
+++ letterOfRecGenerator/app.js
@@ -134,11 +134,25 @@ app.use(function (req, res, next) {
 });
 
 // handle CORS policy
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://recommendation.usc.edu/template-editor/create.TLD"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://recommendation.usc.edu');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // error handler
 app.use(function (err, req, res, next) {
