e115404ebbac5637f9b1bccfa617fcb8031f9f01
Jerry Tejada <jmtejada@usc.edu>
2019-12-15 22:18:17 -0800

    Update app.js

--- letterOfRecGenerator/app.js
+++ letterOfRecGenerator/app.js
@@ -38,32 +38,24 @@ var app = express();
 // app.use(cors());
 
 // Add headers for CORS
-// app.use((req, res, next) => {
-//
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//
-//     if (req.method === 'OPTIONS') {
-//       res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//       return res.status(200).json({});
-//     }
-//
-//     // // Website you wish to allow to connect
-//     // res.setHeader('Access-Control-Allow-Origin', '*');
-//     //
-//     // // Request methods you wish to allow
-//     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//     //
-//     // // Request headers you wish to allow
-//     // res.setHeader('Access-Control-Allow-Headers', 'Authorization');
-//     //
-//     // // Set to true if you need the website to include cookies in the requests sent
-//     // // to the API (e.g. in case you use sessions)
-//     // res.setHeader('Access-Control-Allow-Credentials', true);
-//
-//     // Pass to next layer of middleware
-//     next();
-// });
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 
 var createTemplate = require('./routes/template-editor');
