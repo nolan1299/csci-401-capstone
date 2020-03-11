4d63bea34e288b70f4dd050d57ae1af07f91adbb
Jerry Tejada <jmtejada@usc.edu>
2020-03-11 15:57:10 -0700

    Update template-editor.js

--- letterOfRecGenerator/routes/template-editor.js
+++ letterOfRecGenerator/routes/template-editor.js
@@ -175,7 +175,7 @@ router.post('/create', function (req, res, next) {
     // console.log('Req is: ', req);
 
     var sessionString = JSON.stringify(req.sessionStore.sessions);
-    var id = sessionString.search('_id');
+    var id = sessionString.search('id');
 
     console.log('Includes ID T/F: ', sessionString.includes('_id'));
     console.log('ID: ', id);
