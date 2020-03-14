f360281459f89ca35fe541ebf0e8bfc5ddae7e50
jerrytejada <jmtejada@usc.edu>
2020-03-14 15:52:39 -0700

    Update template-dashboard.js

--- letterOfRecGenerator/routes/template-dashboard.js
+++ letterOfRecGenerator/routes/template-dashboard.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db')
 var fs = require('fs')
+var User = require('../models/user');
 
 /* GET Templates page. */
 router.get('/', function (req, res, next) {
