13b4fad37bbd80dc2c4597af49af5b9310db45a8
jerrytejada <jmtejada@usc.edu>
2020-03-15 15:58:26 -0700

    Update archive.js

--- letterOfRecGenerator/routes/archive.js
+++ letterOfRecGenerator/routes/archive.js
@@ -5,102 +5,102 @@ var Form = require('../models/form');
 
 router.get('/', function (req, res, next) {
 
-  // Searching through session info to find User ID number
-  var sessionString = JSON.stringify(req.sessionStore.sessions);
-  var id_index = sessionString.search('id') + 7;
-  var id_index_lastNum = id_index + 24;
-  var userID = sessionString.slice(id_index, id_index_lastNum);
+  // // Searching through session info to find User ID number
+  // var sessionString = JSON.stringify(req.sessionStore.sessions);
+  // var id_index = sessionString.search('id') + 7;
+  // var id_index_lastNum = id_index + 24;
+  // var userID = sessionString.slice(id_index, id_index_lastNum);
+  //
+  // User.findUser(userID, function (err, user) {
+  //   if (err) {
+  //     console.log('Error finding User.');
+  //   } else {
 
-  User.findUser(userID, function (err, user) {
-    if (err) {
-      console.log('Error finding User.');
-    } else {
-
-      user.getDeactivatedForms( function (err, deactivatedForms) {
+      req.user.getDeactivatedForms( function (err, deactivatedForms) {
           if (err) {
               console.log(err);
           } else {
               res.render('pages/archive', {
                   title: 'Archive',
                   forms: deactivatedForms,
-                  emailtemplates: user.getDeactivatedEmailTemplates(),
-                  templates: user.getDeactivatedTemplates(),
+                  emailtemplates: req.user.getDeactivatedEmailTemplates(),
+                  templates: req.user.getDeactivatedTemplates(),
               });
           }
       });
-    }
-  });
+  //   }
+  // });
 });
 
 router.post('/restore-template', function (req, res, next) {
 
-    // Searching through session info to find User ID number
-    var sessionString = JSON.stringify(req.sessionStore.sessions);
-    var id_index = sessionString.search('id') + 7;
-    var id_index_lastNum = id_index + 24;
-    var userID = sessionString.slice(id_index, id_index_lastNum);
+    // // Searching through session info to find User ID number
+    // var sessionString = JSON.stringify(req.sessionStore.sessions);
+    // var id_index = sessionString.search('id') + 7;
+    // var id_index_lastNum = id_index + 24;
+    // var userID = sessionString.slice(id_index, id_index_lastNum);
+    //
+    // User.findUser(userID, function (err, user) {
+    //   if (err) {
+    //     console.log('Error finding User.');
+    //   } else {
 
-    User.findUser(userID, function (err, user) {
-      if (err) {
-        console.log('Error finding User.');
-      } else {
-
-        user.activateTemplate(req.body.id, function (err) {
+        req.user.activateTemplate(req.body.id, function (err) {
             if (err) {
                 console.log(err);
             } else {
-                user.getDeactivatedForms( function (err, deactivatedForms) {
+                req.user.getDeactivatedForms( function (err, deactivatedForms) {
                     if (err) {
                         console.log(err);
                     } else {
                         res.render('pages/archive', {
                             title: 'Archive Page',
                             forms: deactivatedForms,
-                            emailtemplates: user.getDeactivatedEmailTemplates(),
-                            templates: user.getDeactivatedTemplates(),
+                            emailtemplates: req.user.getDeactivatedEmailTemplates(),
+                            templates: req.user.getDeactivatedTemplates(),
                         });
                     }
                 });
             }
         });
-      }
-    });
+    //   }
+    // });
 });
 
 router.post('/restore-email-template', function (req, res, next) {
 
-  // Searching through session info to find User ID number
-  var sessionString = JSON.stringify(req.sessionStore.sessions);
-  var id_index = sessionString.search('id') + 7;
-  var id_index_lastNum = id_index + 24;
-  var userID = sessionString.slice(id_index, id_index_lastNum);
+  // // Searching through session info to find User ID number
+  // var sessionString = JSON.stringify(req.sessionStore.sessions);
+  // var id_index = sessionString.search('id') + 7;
+  // var id_index_lastNum = id_index + 24;
+  // var userID = sessionString.slice(id_index, id_index_lastNum);
+  //
+  // User.findUser(userID, function (err, user) {
+  //   if (err) {
+  //     console.log('Error finding User.');
+  //   } else {
 
-  User.findUser(userID, function (err, user) {
-    if (err) {
-      console.log('Error finding User.');
-    } else {
 
-
-      user.activateEmailTemplate(req.body.id, function (err) {
+      req.user.activateEmailTemplate(req.body.id, function (err) {
           if (err) {
               console.log(err);
           } else {
-              user.getDeactivatedForms( function (err, deactivatedForms) {
+              req.user.getDeactivatedForms( function (err, deactivatedForms) {
                   if (err) {
                       console.log(err);
                   } else {
                       res.render('pages/archive', {
                           title: 'Archive Page',
                           forms: deactivatedForms,
-                          emailtemplates: user.getDeactivatedEmailTemplates(),
-                          templates: user.getDeactivatedTemplates(),
+                          emailtemplates: req.user.getDeactivatedEmailTemplates(),
+                          templates: req.user.getDeactivatedTemplates(),
                       });
                   }
               });
           }
         });
-      }
-  });
+  //     }
+  // });
 });
 
 module.exports = router;
