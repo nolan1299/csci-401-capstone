864214f983fb3a637d81e9d2a924eeecb9cbdd3f
jerrytejada <jmtejada@usc.edu>
2020-03-16 20:34:56 -0700

    Update response.js

--- letterOfRecGenerator/routes/response.js
+++ letterOfRecGenerator/routes/response.js
@@ -6,19 +6,19 @@ var router = express.Router();
 
 router.get('/', function (req, res, next) {
 
-  // Searching through session info to find User ID number
-  var sessionString = JSON.stringify(req.sessionStore.sessions);
-  var id_index = sessionString.search('id') + 7;
-  var id_index_lastNum = id_index + 24;
-  var userID = sessionString.slice(id_index, id_index_lastNum);
-
-  User.findUser(userID, function (err, user) {
-    if (err) {
-      console.log('Error finding User.');
-    } else {
+  // // Searching through session info to find User ID number
+  // var sessionString = JSON.stringify(req.sessionStore.sessions);
+  // var id_index = sessionString.search('id') + 7;
+  // var id_index_lastNum = id_index + 24;
+  // var userID = sessionString.slice(id_index, id_index_lastNum);
+  //
+  // User.findUser(userID, function (err, user) {
+  //   if (err) {
+  //     console.log('Error finding User.');
+  //   } else {
 
       var questions =  [];
-      user.getForm(req.query.id, function (err, form) {
+      req.user.getForm(req.query.id, function (err, form) {
           if(err) {
               console.log(err)
           } else {
@@ -31,27 +31,27 @@ router.get('/', function (req, res, next) {
               });
           }
       });
-    }
-  });
+  //   }
+  // });
 });
 
 router.get('/update', function (req, res, next) {
 
-  // Searching through session info to find User ID number
-  var sessionString = JSON.stringify(req.sessionStore.sessions);
-  var id_index = sessionString.search('id') + 7;
-  var id_index_lastNum = id_index + 24;
-  var userID = sessionString.slice(id_index, id_index_lastNum);
-
-  User.findUser(userID, function (err, user) {
-    if (err) {
-      console.log('Error finding User.');
-    } else {
+  // // Searching through session info to find User ID number
+  // var sessionString = JSON.stringify(req.sessionStore.sessions);
+  // var id_index = sessionString.search('id') + 7;
+  // var id_index_lastNum = id_index + 24;
+  // var userID = sessionString.slice(id_index, id_index_lastNum);
+  //
+  // User.findUser(userID, function (err, user) {
+  //   if (err) {
+  //     console.log('Error finding User.');
+  //   } else {
 
     var questions =  [];
     var formId = req.query.id;
     var editedResponses = req.query.editedResponses;
-    user.getForm(formId, function (err, form) {
+    req.user.getForm(formId, function (err, form) {
         if(err) {
             console.log(err)
         } else {
@@ -68,8 +68,8 @@ router.get('/update', function (req, res, next) {
             });
           }
       });
-    }
-  });
+  //   }
+  // });
 });
 
 function parseLetter(form) {
