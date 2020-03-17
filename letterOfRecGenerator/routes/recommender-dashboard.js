f7764b184a984f5b60054326436ebabb84a8702b
jerrytejada <jmtejada@usc.edu>
2020-03-16 21:01:20 -0700

    Update recommender-dashboard.js

--- letterOfRecGenerator/routes/recommender-dashboard.js
+++ letterOfRecGenerator/routes/recommender-dashboard.js
@@ -13,10 +13,10 @@ var Docxtemplater = require('docxtemplater');
 var DocxMerger = require('docx-merger');
 var Readable = require('stream').Readable;
 var path = require('path');
-var builder = require('docx-builder');
+var builder = require('docx-builder')
 var docx = new builder.Document();
-var dt = require('./letter-parser');
-var User = require('../models/user');
+var dt = require('./letter-parser')
+//var User = require('../models/user');
 
 "use strict";
 
