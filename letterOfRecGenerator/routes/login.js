32ab195d89dde906247cbd080ab3fe45116617cf
Scott Mai <scottmai@usc.edu>
2020-11-06 14:08:59 -0800

    added auth to more routes

--- letterOfRecGenerator/routes/login.js
+++ letterOfRecGenerator/routes/login.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET login page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
+    console.log('anyone there?')
     res.render('login', {
         title: 'Letter of Recommendation Generator',
         subtitle: '',
